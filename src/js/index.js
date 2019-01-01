ed98a1ecc82181b37aaec204e3b58406905f62ff
hangyida <1921135733@qq.com>
2019-01-01 23:09:07 +0800

    增加接口

--- src/js/index.js
+++ src/js/index.js
@@ -2,6 +2,33 @@ var login = document.querySelector(".login");
 var btns = document.querySelector(".btns");
 var catalogue = document.querySelector("#catalogue");
 var content = document.querySelector(".content");
+init();
+
+function init() {
+    ajax({
+        url: '/render',
+        success: function(data) {
+            render(data.data);
+        }
+    })
+
+    function render(data) {
+        var html = '';
+        data.map(function(i, v) {
+            html += `<dl>
+                         <dt>
+                         <h3>分类名称：${i.name}</h3>
+                         <p>添加时间：${i.time}</p>
+                         </dt>
+                         <dd>
+                             <i class="icon iconfont icon-bianji"></i>
+                             <i class="icon iconfont icon-shanchu"></i>
+                         </dd>
+                     </dl>`
+        })
+        content.innerHTML += html;
+    }
+}
 
 btn.onclick = function() {
     login.classList.remove("niu");
@@ -12,24 +39,19 @@ btns.onclick = function(e) {
     var text = catalogue.value;
     var time = Date.now();
     var times = new Date(time).toLocaleString();
-    console.log(time)
     var html = '';
     if (chil === '取消') {
         login.classList.add("niu");
         catalogue.value = '';
 
     } else if (chil === '保存') {
-        html += ` <dl>
-                     <dt>
-                     <h3>分类名称：${text}</h3>
-                     <p>添加时间：${times}</p>
-                     </dt>
-                     <dd>
-                         <i class="icon iconfont icon-bianji"></i>
-                         <i class="icon iconfont icon-shanchu"></i>
-                     </dd>
-                  </dl>`
-        content.innerHTML += html;
+        ajax({
+            url: `/create?name=${text}&time=${times}`,
+            success: function(data) {
+                content.innerHTML = '';
+                init();
+            }
+        })
         catalogue.value = '';
         login.classList.add("niu");
     }
